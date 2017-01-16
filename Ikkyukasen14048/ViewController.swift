ae9e2c75af70888d10462fa372ed33a2b942f070
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-01-16 20:34:26 +0900

    Initial Commit

---
+++ Ikkyukasen14048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ikkyukasen14048
+//
+//  Created by Keisei Saito on 2017/01/16.
+//  Copyright © 2017 keisei_1092. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
