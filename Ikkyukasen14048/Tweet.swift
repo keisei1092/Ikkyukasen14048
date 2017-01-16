54170127a9317df9ccd06f68ad8747a5ced94634
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-01-16 20:37:00 +0900

    create Tweet class

---
+++ Ikkyukasen14048/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  Ikkyukasen14048
+//
+//  Created by Keisei Saito on 2017/01/16.
+//  Copyright © 2017 keisei_1092. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+
+}
