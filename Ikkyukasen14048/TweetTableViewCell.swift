ee458cd8eaf93bb3a2799f532b7068cf4113f54d
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-01-16 22:11:15 +0900

    add TweetTableViewCell

---
+++ Ikkyukasen14048/TweetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetTableViewCell.swift
+//  Ikkyukasen14048
+//
+//  Created by Keisei Saito on 2017/01/16.
+//  Copyright © 2017 keisei_1092. All rights reserved.
+//
+
+import UIKit
+
+final class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
